9d78c58a05514637e9c10321a65ad9f464a26b75
PauloEd-hub <pauloedu.cavalcante@gmail.com>
2022-12-15 02:26:58 -0300

    Class about Dates in java.

---
+++ src/RDatas/test/DateTest01.java
@@ -0,0 +1,12 @@
+package RDatas.test;
+
+import java.util.Date;
+
+public class DateTest01 {
+    public static void main(String[] args) {
+        Date date = new Date(1_000_000_000_0L); //long 100000
+        System.out.println(date);
+
+
+    }
+}
